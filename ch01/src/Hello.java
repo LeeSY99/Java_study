835778e29e68a49699e1a2dfe604369f7ef892bf
LeeSY99 <101381138+LeeSY99@users.noreply.github.com>
2022-05-04 00:07:23 +0900

    스터디 연습

---
+++ ch01/src/Hello.java
@@ -0,0 +1,22 @@
+
+public class Hello {
+
+	System.out.println("Hello123456 world");
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//컨트롤+d 한줄 삭제
+//		ctrl+alt+shift+down 행단위 복사
+//		alt+ up, down 행단위 이동
+//		ctrl + i 자동 들여쓰기
+//		ctrl + / 주삭
+//		ctrl + space 자동완성
+		System.out.println();
+		System.out.println();
+		System.out.println("Hello123456 world");
+		System.out.println("Hello123456 world");
+		System.out.println("Hello123456 world");
+		System.out.println("Hello123456 world");
+		System.out.println("Hello123456 world");
+	}
+	
+}
