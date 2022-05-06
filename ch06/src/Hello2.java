9fefc37a31a5847f43acf90902f1ff7eff605238
이상윤 <101381138+LeeSY99@users.noreply.github.com>
2022-05-07 01:24:33 +0900

    Add files via upload

---
+++ ch06/src/Hello2.java
@@ -0,0 +1,9 @@
+
+public class Hello2 {//소스파일의 이름은 public class와 일치
+					//하나의 소스파일에는 public 1개만
+	public static void main(String[] args) {
+		//main이 있는 클래스이름과 파일이름이 같아야함
+	}
+}
+class hello3{}
+class hello4{}
